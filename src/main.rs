15873963f64298a2ef6da1fa1280df0a51624ca5
Arjan Bal <arjansbal@google.com>
2023-01-28 01:07:38 +0530

    Create a REPL to tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use lexer::Lexer;
+use std::io::{stdin, stdout, Write};
+
 mod lexer;
 mod token;
 
+const PROMPT: &str = ">>";
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("{} ", PROMPT);
+        stdout().flush().unwrap();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+        let lex = Lexer::new(&input);
+        for token in lex {
+            println!("{:?}", token);
+        }
+    }
 }
